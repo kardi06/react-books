4b3f6295b51ea01816c15cd2d66bb5bb0c7d2199
Kardi <kgland06@gmail.com>
2024-09-04 19:01:35 +0700

    context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import axios from "axios";
 function App() {
     const [books,setBooks] = useState([]);
 
-    const editBookById = (id, newTitle) => {
+    const fetchBook = async () => {
+        const response = await axios.get("http://localhost:3001/books");
+        setBooks(response.data);
+    }
+
+    useEffect(()=>{
+        fetchBook();
+    },[]);
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`,{
+            title: newTitle
+        });
+
         const updateBook = books.map((book) => {
             if (book.id === id) {
-                return {...book, title: newTitle};
+                return {...book, ...response.data};
             }
             return book;
         })
         setBooks(updateBook);
     }
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`)
         const updateBook = books.filter((book)=>{
             return book.id !== id;
         })
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,18 @@
+import { useContext } from "react";
 import BookShow from "./BookShow";
+import BooksContext from "../context/books";
+
 function BookList({books,onDelete,onEdit}){
+    const value = useContext(BooksContext);
+
     const renderedBooks = books.map((book)=>{
         return <BookShow onEdit={onEdit} key={book.id} book={book} onDelete={onDelete}/>
     })
     return (
-        <div className="book-list">{renderedBooks}</div>
+        <div className="book-list">
+            {/* {value} */}
+            {renderedBooks}
+        </div>
     )
 }
 
